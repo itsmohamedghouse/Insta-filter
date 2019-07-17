0891e16dc2637509ad3a852cd124b22aa357d287
mohamed <ghouse.mohamedsm@gmail.com>
2019-07-17 20:10:05 +0530

    third image

--- instafilter.py
+++ instafilter.py
@@ -4,6 +4,7 @@ from PIL import Image
 # load images into objects
 base_img=Image.open("4.jpg")
 img_filter=Image.open("filter.jpg")
+third_image=Image.open("4.jpg")
 
 #set image size
 size=(760,760)
@@ -11,6 +12,7 @@ base_img=base_img.resize(size)
 img_filter=img_filter.resize(size)
 r,g,b=base_img.split()
 R,G,B=img_filter.split()
+Rr,Gg,Bb=third_image.split()
 im=Image.merge("RGB",(r,g,B))
 # im=Image.merge("HSV",(R,g,B))
 im.show()
